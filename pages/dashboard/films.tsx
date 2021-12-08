da30c104d19391bff248d6f9f2804ac07ccceeb5
Justin Elmore <jelmore1674@pm.me>
2021-12-08 17:20:10 -0600

    cleaned up old imports

--- pages/dashboard/films.tsx
+++ pages/dashboard/films.tsx
@@ -78,7 +78,7 @@ export default function FilmTable({ films }: FilmsProps) {
 
 	function truncateString(str: string, num: number): string {
 		if (str.length > num) {
-			return str.slice(0, num) + '...';
+			return str.slice(0, num) + '....';
 		} else {
 			return str;
 		}
